ec368bd1ae221ed2ae90c8aef8dcd08890813d6b
Mazen Walid Fikry <mazen.walid251@gmail.com>
2024-09-24 19:49:25 +0300

    Update app.py

--- Week-9/birthdays/app.py
+++ Week-9/birthdays/app.py
@@ -1,35 +1,43 @@
-from cs50 import SQL
-from flask import Flask, request, redirect, render_template
+import os
 
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+# Configure application
 app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-database = SQL("sqlite:///birthdays.db") # /// not a typo that's how it is written
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///birthdays.db")
+
 
 @app.after_request
-def no_cache(response):
+def after_request(response):
+    """Ensure responses aren't cached"""
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = "0"
+    response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    if request.method == "POST":
-        fname = request.form.get("first_name")
-        lname = request.form.get("last_name")
-        day = request.form.get("day")
-        month = request.form.get("month")
-        year = request.form.get("year")
 
-        database.execute("INSERT INTO birthdays (first_name, last_name, day, month, year) VALUES (?, ?, ?, ?, ?)", fname, lname, day, month, year)
-        # ? to prevent injection attacks from the client side
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+
+        # TODO: Add the user's entry into the database
+
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
         return redirect("/")
 
     else:
-        entries = database.execute("SELECT * FROM birthdays")
-        return render_template("index.html", birthdays=entries)
 
-
-
-
+        # TODO: Display the entries in the database on index.html
+        
+        birthdays = db.execute("SELECT * FROM birthdays")
+        return render_template("index.html", birthdays=birthdays)
